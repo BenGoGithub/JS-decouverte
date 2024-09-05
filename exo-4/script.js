1f4adb73b4da342ce52f228364f7c90b274cafcb
Ben <benjamin.guenoun@le-campus-numerique.fr>
2024-09-05 11:37:08 +0200

    Ajout de l'exercice 4

--- exo-4/script.js
+++ exo-4/script.js
@@ -3,3 +3,26 @@
 console.log('exo-4');
 
 var secretMessage = ["Learning", "isn't", "about", "what", "you", "get", "easily", "the", "first", "time,", "it's", "about", "what", "you", "can", "figure", "out.", "-2015,", "Chris", "Pine,", "Learn", "JavaScript"];
+
+console.table(secretMessage);
+
+// Enlever le dernier élément
+secretMessage.pop();
+
+// Ajouter 'to' et 'program' à la fin
+secretMessage.push("to", "program");
+
+// Changer 'easily' par 'right'
+secretMessage[secretMessage.indexOf("easily")] = "right";
+
+// Supprimer le premier élément
+secretMessage.shift();
+
+// Ajouter "Programming" au début
+secretMessage.unshift("Programming");
+
+// Remplacer 'get', 'right', 'the', 'first', 'time' par 'know'
+secretMessage.splice(secretMessage.indexOf("get"), 5, "know");
+
+// Afficher le message secret
+console.log(secretMessage.join(" "));
